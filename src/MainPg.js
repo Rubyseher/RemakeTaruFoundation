29653a208485d369bc7c1a9802acbb3e5aaeaf33
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2022-07-24 14:57:54 +0530

    edit floating animation

--- src/MainPg.js
+++ src/MainPg.js
@@ -3,7 +3,7 @@ import './App.css'
 function MainPg() {
     return (
         <div>
-            <img src="/img/front2.png" width={500} style={{margin:'0px 90px'}} className='floating-img'/>
+            <img src="/img/front2.png" width={500} style={{margin:'40px 90px'}} className='floating-img'/>
             <img src="/img/healthcare.png" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <img src="/img/forAll.png"  />
         </div>
