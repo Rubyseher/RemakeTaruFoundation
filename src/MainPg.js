4cce5156eb66ad0dca80a0e0499586519aa49fb3
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2022-07-26 14:24:10 +0530

    added mongodb get post + axios + fetching data

--- src/MainPg.js
+++ src/MainPg.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css'
 function MainPg() {
+    
     return (
         <div>
             <img src="/img/front2.png" width={500} style={{margin:'40px 90px'}} className='floating-img'/>
--- src/RmpDashboard.js
+++ src/RmpDashboard.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RmpNotification, MultipurposeButton } from './components'
+import axios from "./axios.js";
+
 function RmpDashboard() {
+    const [Notification, setNotification] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await axios.get('/rmp')
+            console.log(data);
+            setNotification(data)
+        }
+        fetchData();
+    }, [])
     return (
         <div>
 
-            <div style={{ display: 'flex', backgroundColor: ' var(--lightBlue)',padding:'30px 60px'}}>
+            <div style={{ display: 'flex', backgroundColor: ' var(--lightBlue)', padding: '30px 60px' }}>
                 <div >
                     <h3 >Notification</h3>
-                    <div style={{ display: 'inline-flex' ,width:'900',flexFlow:'wrap',overflow:'scroll',height:'230px'}}>
-                        <RmpNotification />
-                        <RmpNotification />
-                        <RmpNotification />
-                        <RmpNotification />
-                        <RmpNotification />
-                        <RmpNotification />
+                    <div style={{ display: 'inline-flex', width: '900', flexFlow: 'wrap', overflow: 'scroll', height: '230px' }}>
+                        {
+                            Notification.length!=0 && Array.from(Notification).forEach((item, i) => (
+                                <RmpNotification />
+                                // console.log("onw")
+                            ))
+                        }
                     </div>
                 </div>
                 <img src='./img/rmp.svg' width='350' height='350' style={{ float: 'right' }} />
