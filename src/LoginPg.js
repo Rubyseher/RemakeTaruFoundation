b0572a05947e8111735d80ad311b2bc266dc681c
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2023-01-21 11:17:12 +0530

    getting user data from mongo loginpg

--- src/LoginPg.js
+++ src/LoginPg.js
@@ -1,18 +1,29 @@
-import React, { useState } from "react";
-// how do i make multiple users registered
+import React, { useState ,useEffect} from "react";
 import Form from 'react-bootstrap/Form'
 import './Login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
 import Button from 'react-bootstrap/Button';
+import axios from './axios.js';
 
 function LoginPg() {
-    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
+
+    const [users, setUsers] = React.useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const db = await axios.get('/login')
+            console.log(db.data);
+            setUsers((oldState) => db.data)
+        }
+        fetchData();
+    }, [])
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(`The name you entered was: ${name}, ${password}`)
+        console.log(`The name you entered was: ${phone}, ${password}`)
     }
 
     return (
@@ -21,8 +32,8 @@ function LoginPg() {
                 <div style={{ borderRadius: 20, width: '28%' ,backgroundColor:'white',padding:'30px'}} className='shadow'>
                     <form onSubmit={handleSubmit}>
                         <img alt="" src="/img/logoGlow.png" class='centerlogo' />
-                        <input type="text" name="email" placeholder="Phone Number" value={name}
-                            onChange={(e) => setName(e.target.value)} />
+                        <input type="text" name="phone" placeholder="Phone Number" value={phone}
+                            onChange={(e) => setPhone(e.target.value)} />
                         <input type="password" name="password" placeholder="Password" value={password}
                             onChange={(e) => setPassword(e.target.value)} />
                         <div class="forgot-remember">
