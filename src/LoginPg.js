b920cb9f3a13735da65fad25470d471a2124cadf
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2023-01-24 20:22:09 +0530

    login and redirect

--- src/LoginPg.js
+++ src/LoginPg.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form'
 import './Login.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
-import Button from 'react-bootstrap/Button';
 import axios from './axios.js';
-import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function LoginPg() {
     const [phone, setPhone] = useState("");
@@ -18,19 +16,32 @@ function LoginPg() {
 
     const [signIn, setSignIn] = React.useState(true)
 
-    useEffect(() => {
-        // async function fetchData() {
-        //     const db = await axios.get('/login')
-        //     console.log(db.data);
-        //     setUsers((oldState) => db.data)
-        // }
-        // fetchData();
-    }, [])
+    const navigate = useNavigate();
+    const routeChange = () => {
+        let path = `/patient`;
+        navigate(path, {
+            state: {
+                specialization: "hi"
+            }
+        });
+    }
 
-    async function handleSubmit(event) {
+    async function handleSignIn(event) {
+        event.preventDefault();
+        console.log(phone, password)
+        const res = await axios.post('/login', {
+            phone,
+            password,
+        }).then(function (response) {
+            routeChange()
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+    async function handleSignUp(event) {
         event.preventDefault();
         console.log(phone, password, fullName, age, bloodGroup)
-        const res = await axios.post('/login', {
+        const res = await axios.post('/register', {
             fullName,
             age,
             bloodGroup,
@@ -49,59 +60,58 @@ function LoginPg() {
         <div className="cornerBg">
             <div style={{ padding: '110px 0 0 120px' }}>
                 <div style={{ borderRadius: 20, width: '28%', backgroundColor: 'white', padding: '30px' }} className='shadow'>
-                    <form onSubmit={handleSubmit}>
-                        {
-                            signIn == true ?
-                                <Fragment>
-                                    <img alt="" src="/img/logoGlow.png" class='centerlogo' />
+                    {
+                        signIn == true ?
+                            <form onSubmit={handleSignIn}>
+                                <img alt="" src="/img/logoGlow.png" class='centerlogo' />
 
-                                    <input type="text" name="phone" placeholder="Phone Number" value={phone}
-                                        onChange={(e) => setPhone(e.target.value)} />
+                                <input type="text" name="phone" placeholder="Phone Number" value={phone}
+                                    onChange={(e) => setPhone(e.target.value)} />
 
-                                    <input type="password" name="password" placeholder="Password" value={password}
-                                        onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" name="password" placeholder="Password" value={password}
+                                    onChange={(e) => setPassword(e.target.value)} />
 
-                                    <div class="forgot-remember">
-                                        <div class="forgot"> <a href="#">Forgot Password?</a> </div>
-                                    </div>
+                                <div class="forgot-remember">
+                                    <div class="forgot"> <a href="#">Forgot Password?</a> </div>
+                                </div>
 
-                                    <input type="submit" name="send" value="SIGN IN" />
+                                <input type="submit" name="send" value="SIGN IN" />
 
-                                    <div class="createAcc" style={{ textAlign: 'center' }}>
-                                        <div class="createAcc" onClick={() => setSignIn(false)}> <a href="#">Dont have an Account?<br />Create one Now</a> </div>
-                                    </div>
-                                </Fragment>
+                                <div class="createAcc" style={{ textAlign: 'center' }}>
+                                    <div class="createAcc" onClick={() => setSignIn(false)}> <a href="#">Dont have an Account?<br />Create one Now</a> </div>
+                                </div>
+                            </form>
 
-                                :
-                                <Fragment>
-                                    <img alt="" src="/img/logoGlow.png" class='centerlogo' />
+                            :
+                            <form onSubmit={handleSignUp}>
+                                <img alt="" src="/img/logoGlow.png" class='centerlogo' />
 
-                                    <input type="text" name="phone" placeholder="Phone Number" value={phone}
-                                        onChange={(e) => setPhone(e.target.value)} />
+                                <input type="text" name="phone" placeholder="Phone Number" value={phone}
+                                    onChange={(e) => setPhone(e.target.value)} />
 
-                                    <input type="password" name="password" placeholder="Password" value={password}
-                                        onChange={(e) => setPassword(e.target.value)} />
+                                <input type="password" name="password" placeholder="Password" value={password}
+                                    onChange={(e) => setPassword(e.target.value)} />
 
-                                    <input type="text" name="name" placeholder="Full Name" value={fullName}
-                                        onChange={(e) => setFullName(e.target.value)} />
+                                <input type="text" name="name" placeholder="Full Name" value={fullName}
+                                    onChange={(e) => setFullName(e.target.value)} />
 
-                                    <div >
-                                        <input style={{ width: '45%', marginRight: 18, display: "inline-block" }} type="text" name="age" placeholder="Age" value={age}
-                                            onChange={(e) => setAge(e.target.value)} />
-                                        <input style={{ width: '49%', display: "inline-block" }} type="text" name="text" placeholder="Blood Group" value={bloodGroup}
-                                            onChange={(e) => setBloodGroup(e.target.value)} />
-                                    </div>
+                                <div >
+                                    <input style={{ width: '45%', marginRight: 18, display: "inline-block" }} type="text" name="age" placeholder="Age" value={age}
+                                        onChange={(e) => setAge(e.target.value)} />
+                                    <input style={{ width: '49%', display: "inline-block" }} type="text" name="text" placeholder="Blood Group" value={bloodGroup}
+                                        onChange={(e) => setBloodGroup(e.target.value)} />
+                                </div>
 
-                                    <input type="text" name="address" placeholder="Address" value={address}
-                                        onChange={(e) => setAddress(e.target.value)} />
+                                <input type="text" name="address" placeholder="Address" value={address}
+                                    onChange={(e) => setAddress(e.target.value)} />
 
-                                    <input type="date" name="dob" placeholder="Date of Birth" value={dob}
-                                        onChange={(e) => setDob(e.target.value)} />
+                                <input type="date" name="dob" placeholder="Date of Birth" value={dob}
+                                    onChange={(e) => setDob(e.target.value)} />
 
-                                    <input type="submit" name="send" value="SIGN UP" />
-                                </Fragment>
-                        }
-                    </form>
+                                <input type="submit" name="send" value="SIGN UP" />
+                            </form>
+                    }
+
                 </div>
             </div>
             <img src='/img/login2.svg' className="login-img" />
