92999667046c9c6c89dbbb8d6ef26a674bad75be
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2023-02-06 19:47:54 +0530

    env

--- src/Booking.js
+++ src/Booking.js
@@ -29,7 +29,7 @@ function Booking(props) {
       setNotification((oldState) => newAr)
     }
     fetchData();
-    console.log(window.localStorage.getItem("token") );
+    // console.log(window.localStorage.getItem("token") );
   }, [])
 
   return (
