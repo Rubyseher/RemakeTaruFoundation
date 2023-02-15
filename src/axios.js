513189bd5dc4124f2014553c3d3d26736d47dbed
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2023-02-15 22:19:06 +0530

    base url

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const instances = axios.create({
-    baseURL:"http://localhost:8001"
-    // baseURL:"https://tarufoundationremakebackend.netlify.app"
+    // baseURL:"http://localhost:8001"
+    baseURL:"https://tarufoundationremakebackend.netlify.app"
 })
 
 export default instances;
