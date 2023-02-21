bf4f4320690d3cc4ca30f91302446ea7f4c9da93
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2023-02-21 21:49:04 +0530

    added render

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instances = axios.create({
-    baseURL:"http://localhost:8001"
+    baseURL:"https://tarubackend.onrender.com"
+    // baseURL:"http://localhost:8001"
     // baseURL:"https://tarufoundationremakebackend.netlify.app"
 })
 
